a550e6905216f6b7f816934a0a6ee5bf13d9040e
kavinkishore6 <72402904+kavinkishore6@users.noreply.github.com>
2025-05-20 17:42:55 +0530

    Update Question6.java

--- Question6.java
+++ Question6.java
@@ -1,17 +1,17 @@
 public class Question6 {
     public static void main(String[] args) {
-        int n = 5;  // Total number of rows
+        int n = 5;  
 
         for (int i = 1; i <= n; i++) {
             int num = 5;
 
-            // First part: decreasing numbers
+            
             for (int j = 1; j <= i; j++) {
                 System.out.print(num + " ");
                 num--;
             }
 
-            // Second part: repeat the last printed number
+            
             for (int k = i + 1; k <= n; k++) {
                 System.out.print((num + 1) + " ");
             }
